78f5904ea768cbc8bee19fb285feb793474135e1
Jon Ursenbach <jon@ursenba.ch>
2021-08-26 18:38:27 -0700

    feat: minor rewrite to support friendly configuration

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# Auto-milestone Testing
-Building and testing a GitHub action to auto-open milestones on a schedule.
+# Scheduled Milestones
+
+A GitHub action that can auto-create weekly milestones.
+
+The folks at [ReadMe](https://readme.com/) utilize this to internally organize our weekly releases.
+
+This action is similar to [github-action-milestone-schedule](https://github.com/MrGossett/github-action-milestone-schedule) but offers some friendlier configuration options.
+
+## Options
+
+This action supports the following options:
+
+### title
+
+A prefix for your scheduled milestone. All created milestones will look like `Up for Review (August 27)`, where "Up for Review" is your title.
+
+* *Required*: `Yes`
+* *Type*: `string`
+* *Example*: `Up for Review`
+
+### days
+
+A comma-separated list of the days of the upcoming week that you want milestones for. For example if you want milestones for every Tuesday and Thursday of the week you would set this to `Tuesday,Thursday`. Just Tuesday? Set it to `Tuesday`. Fridays? I mean I guess if you're living on the edge, then `Friday`.
+
+* *Required*: `Yes`
+* *Type*: `string`
+
+### date_options
+
+This option allows you to control how the dates in your milestone titles should look. The options here should be a JSON-encoded string of the options that you would normally supply to `Date.toLocaleDateString()`. Check out the [docs on MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString) for `toLocaleDateString` for details.
+
+Note: If you do not configure this, the default format for dates in in your milestones will look like `M/D/YYYY`.
+
+* *Required*: `Yes`
+* *Type*: `string`
+
+## Output
+
+### milestones
+
+If any milestones were created, this will contain an array of their URLs.
+
+## Example
+
+```yaml
+name: Weekly Milestones
+on:
+  schedule:
+    - cron: '0 0 * * SUN' # Run every Sunday at midnight
+
+jobs:
+  generate:
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Schedule Milestones
+        uses: readmeio/scheduled-milestones@v1
+        id: scheduled
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+          title: Up for Review
+          days: Tuesday,Thursday
+          date_options: '{"month": "long", "day": "numeric"}'
+
+      - name: Created Milestones
+        run: echo ${{ steps.scheduled.outputs.milestones }}
+```
--- index.js
+++ index.js
@@ -1,40 +1,59 @@
-const github = require("@actions/github");
-const core = require("@actions/core");
+const github = require('@actions/github');
+const core = require('@actions/core');
+const datefns = require('date-fns');
 
-// github.context.payload // JSON for the trigger event
-// core.getInput('who-to-greet');
-// core.setOutput("time", time);
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+async function run() {
+  const token = core.getInput('token');
+  const octokit = github.getOctokit(token);
+
+  const baseTitle = core.getInput('title');
+  const days = core.getInput('days').split(',');
+  const dateOpts = JSON.parse(core.getInput('date_options'));
+
+  const promises = [];
+
+  days.forEach(day => {
+    promises.push(
+      new Promise((resolve, reject) => {
+        const date = datefns[`next${capitalize(day)}`](new Date());
+        const milestoneDate = date.toLocaleDateString(undefined, dateOpts);
+
+        const title = `${baseTitle} (${milestoneDate})`;
+
+        octokit.rest.issues
+          .createMilestone({
+            ...github.context.repo,
+            title,
+            due_on: date,
+          })
+          .then(({ data }) => {
+            resolve(data.html_url);
+          })
+          .catch(err => {
+            // If the milestone already exists `err.message` will look like the following:
+            //    'Validation Failed: {"resource":"Milestone","code":"already_exists","field":"title"}'
+            if (!err.message.match(/already_exists/)) {
+              reject(err);
+            }
+          });
+      })
+    );
+  });
+
+  const milestones = [];
+  await Promise.all(promises).then(url => {
+    milestones.push(url);
+  });
+
+  core.setOutput('milestones', milestones);
+}
 
 try {
-  const token = core.getInput("token");
-  /* This should be a token with access to your repository scoped in as a secret.
-   * The YML workflow will need to set myToken with the GitHub Secret Token
-   * myToken: ${{ secrets.GITHUB_TOKEN }}
-   * https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-   */
-
-  const octokit = github.getOctokit(token);
-  /* You can also pass in additional options as a second parameter to getOctokit
-   * const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
-   */
-
-  let title = core.getInput("title");
-  const timeBetween = parseInt(core.getInput("time_between"));
-  let date = new Date();
-  date.setDate(date.getDate() + timeBetween);
-  const dateOpts = JSON.parse(core.getInput("date_options"));
-  date = date.toLocaleDateString(undefined, dateOpts);
-
-  title = `${title} (${date})`;
-
-  octokit.rest.issues
-     .createMilestone({
-      ...github.context.repo,
-      title,
-    })
-    .then(({ data: pullRequest }) => {
-      console.log(pullRequest);
-    });
+  run();
 } catch (err) {
   core.setFailed(err.message);
 }
